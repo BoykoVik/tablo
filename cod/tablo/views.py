67457855a19712876a4a34a7082e914d4f773158
BoykoVik <gexar47@gmail.com>
2023-03-02 00:41:33 +0300

    1

--- cod/tablo/views.py
+++ cod/tablo/views.py
@@ -1,5 +1,18 @@
 from django.shortcuts import render
-
+from tablo.models import Holidays, Slides, Runningrow
 # Create your views here.
 def index(request):
-    return render(request, 'tablo/tablo.html')
+    slides = Slides.objects.filter(isactive = True)
+    holids = Holidays.objects.filter(isactive = True)
+    rows = Runningrow.objects.filter(isactive = True)
+    row = ''
+    for el in rows:
+        row = row + el.about + '         '
+    poppe = []
+    for b in holids:
+        b.month = b.get_month_display()
+        poppe.append(b)
+    return render(request, 'tablo/tablo.html', {
+        'slides': slides,
+        'holids': poppe,
+        'row': row})
